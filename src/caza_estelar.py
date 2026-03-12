f973d04d145a24ec21d77d4b4d23dc8286bb1be0
Santiago Moreno <162921866+HelloWord-404@users.noreply.github.com>
2026-03-12 20:44:25 +0100

    Fix syntax errors and improve attack logic

--- src/caza_estelar.py
+++ src/caza_estelar.py
@@ -5,25 +5,28 @@ class Caza_Espacial:
     def __init__(self, dotacion: int, estado_ataque: bool):
         self.__dotacion = dotacion
         self.estado_ataque = estado_ataque
-        
+       # self.ubicacionAtaques 
       
     def mostrar_info(self):
         print(self)
 
     def isPossible_Star_Attack(self, ubicacionAtaque: Ubicacion) -> bool:
-      if(self.__dotacion >100 && ubicacionAtaque != Uubicacion.ENDOR)
+      if(self.__dotacion >100 and ubicacionAtaque != Ubicacion.ENDOR):
         return True
-      else
+      else:
         valor = random.choice([True, False])
         return valor
         
     def iniciar_ataque(self, ubicacionAtaque: Ubicacion) -> str:
-        if(self.isPossible_Star_Attack())
-          self.estado_ataque = True
-        else
-         self.estado_ataque = False
-        if(estado_ataque) return( print("El ataque fue exitoso"))
+       # self.ubicacionAtaques = ubicacionAtaque
+        if(self.isPossible_Star_Attack(ubicacionAtaque)):
+            self.estado_ataque = True
+        else:
+            self.estado_ataque = False
+        if(self.estado_ataque): return( "El ataque fue exitoso")
+        else: return( "El ataque fallo o no fue posible")
       
     
     def __str__(self):
         return f" -> {self.estado_ataque} --> "
+
